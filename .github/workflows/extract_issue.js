ee184b386195cabd3cf879434cbbb1ba33ab3a0b
Santiago Trujillo Zuluaga <stztrujillo@hotmail.com>
2023-07-30 17:14:00 -0500

    Update extract_issue.js

--- .github/workflows/extract_issue.js
+++ .github/workflows/extract_issue.js
@@ -7,9 +7,8 @@ function extractSection(content, sectionHeading) {
 }
 
 function main() {
-  const title = process.env.INPUT_TITLE;
-  const body = process.env.INPUT_BODY;
-  const originRepoUrl = `${process.env.GITHUB_SERVER_URL}/${process.env.GITHUB_REPOSITORY}`;
+  const title = process.argv[2];
+  const body = process.argv[3];
 
   const bugDescription = extractSection(body, "### 1. **Bug/Vulnerability Description**");
   const hardwareSoftwareSpecs = extractSection(body, "### 2. **Hardware and Software Specifications**");
@@ -19,7 +18,7 @@ function main() {
   const chooseTheRightLabel = extractSection(body, "### 6. **Choose the Right Label**");
   const additionalContext = extractSection(body, "### 7. **Additional Context**");
 
-  const bodyWithLink = `**Bug/Vulnerability Description:**\n${bugDescription}\n\n**Hardware and Software Specifications:**\n${hardwareSoftwareSpecs}\n\n**Steps to Reproduce:**\n${stepsToReproduce}\n\n**Impact Analysis:**\n${impactAnalysis}\n\n**Code Fix Submission:**\n${codeFixSubmission}\n\n**Choose the Right Label:**\n${chooseTheRightLabel}\n\n**Additional Context:**\n${additionalContext}\n\nComments are not being updated\nOriginal Issue can be found [here](${originRepoUrl})`;
+  const bodyWithLink = `**Bug/Vulnerability Description:**\n${bugDescription}\n\n**Hardware and Software Specifications:**\n${hardwareSoftwareSpecs}\n\n**Steps to Reproduce:**\n${stepsToReproduce}\n\n**Impact Analysis:**\n${impactAnalysis}\n\n**Code Fix Submission:**\n${codeFixSubmission}\n\n**Choose the Right Label:**\n${chooseTheRightLabel}\n\n**Additional Context:**\n${additionalContext}\n\nComments are not being updated\nOriginal Issue can be found [here]`;
 
   console.log(bodyWithLink);
 }
